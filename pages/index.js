14d8473a0e37a183bbe12a214b59d5ffe0e33492
Njunge <njungedev@gmail.com>
2019-10-01 01:11:34 +0300

    Revert "feat(axios): add functionality to query for movies"
    This reverts commit 3ba8c471582c04c823735b98b72c9f840531dc86.

--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,7 @@
-import React, { Component } from "react";
-import search from "../src/utils";
+const Index = () => (
+    <div>
+        <p>Hello Next.js</p>
+    </div>
+)
 
-export default class Index extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { movies: {}, loading: false, value: "" };
-    this.queryForMovies = this.queryForMovies.bind(this);
-    this.apiKey = "e3963faaff14ce2935d481ee7d0179fb";
-  }
-
-  async queryForMovies(queryText) {
-    this.setState({ value: queryText, loading: true });
-    const url = `https://api.themoviedb.org/3/search/movie?query=${queryText}&api_key=${this.apiKey}`;
-    const results = await search(url);
-    console.log("the results", results);
-  }
-  render() {
-    return (
-      <div>
-        <input
-          type="text"
-          placeholder="Search for..."
-          value={this.state.value}
-          onChange={event => this.queryForMovies(event.target.value)}
-        />
-      </div>
-    );
-  }
-}
+export default Index
